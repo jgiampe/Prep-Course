6d99872f78bb06214626c4c8c2a4983fca148731
Julian Giampetruzzi <jggiampe@gmail.com>
2022-08-03 20:47:00 -0300

    Homework JS 10

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,14 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  let matriz=[];
+  let i=0;
+  for(let prop in objeto)
+    {
+      matriz[i]= [prop,objeto[prop]];
+      i++;
+    }
+  return matriz;
 }
 
 
@@ -18,6 +26,23 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  let obj={};
+  let cuenta;
+  let letra;
+ 
+  while(string.length)
+  {
+    cuenta=1;
+    letra=string[0];
+    for(let i=1; i<string.length; i++)
+      {
+        if(string[i]===letra)
+          cuenta++;
+      }
+    obj[letra]=cuenta;
+    string=string.replace(new RegExp(letra,'g'),'');
+  }
+  return obj;
 }
 
 
@@ -26,6 +51,19 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  let string='';
+  for(let i=0; i<s.length;i++)
+  {
+    if(s[i]===s[i].toUpperCase())
+      {
+        string+=s[i];
+        s=s.replace(s[i],'');
+        i--;                    //porque eliminé un elemento del string
+      }
+  }
+  for(let i=0; i<s.length;i++)
+    string+=s[i];
+  return string;
 }
 
 
@@ -35,6 +73,35 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  let palabra='', reves='';
+  let frase = '';
+  
+  for(let i=0; i<str.length; i++)
+  {
+    if(i!==str.length-1)
+    {
+      if(str[i]!=' ')
+        palabra+=str[i];
+      else
+      {
+       for(let j=palabra.length-1; j>=0; j--)
+          reves+=palabra[j];
+        frase+=reves+' ';
+        palabra = '';
+        reves='';
+      } 
+    }
+    else
+      {
+        palabra+=str[i];
+        for(let j=palabra.length-1; j>=0; j--)
+          reves+=palabra[j];
+        frase+=reves;
+      }
+    
+  }
+  return frase;
+  
 } 
 
 
@@ -43,6 +110,16 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  let palabra = numero.toString();
+  
+  for (let i=0; i<Math.floor(palabra.length/2); i++)
+    { //console.log(i);
+      if(palabra[i]!==palabra[palabra.length-1-i])
+         // console.log('ok');
+          return'No es capicua';
+    }
+  return 'Es capicua';
+  
 }
 
 
@@ -50,6 +127,15 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  for(let i=0; i<cadena.length; i++)
+  {
+    if(cadena[i]==='a' || cadena[i]==='b' || cadena[i]==='c')
+    {
+      cadena=cadena.replace(cadena.charAt(i),'');
+      i--;
+    }
+  }
+  return cadena;
 }
 
 
@@ -57,6 +143,15 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  for(let i=0; i<arr.length-1; i++)
+  {
+    for(let j=0; j<arr.length-i-1;j++)
+    {
+      if(arr[j].length > arr[j+1].length)
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+    }
+  }
+  return arr;
 }
 
 
@@ -66,6 +161,12 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  let arr=[];
+  for(let i=0; i<arreglo1.length; i++)
+    for(let j=0;j<arreglo2.length; j++)
+      if(arreglo1[i]===arreglo2[j])
+        arr.push(arreglo1[i])
+  return arr;
 }
 
 
